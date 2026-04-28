6b51cd83c4bda232412e3aca13ccebd6139f0c35
Sonny Piers <sonny@fairscope.com>
2026-04-28 13:24:28 +0000

    document rauc updates

--- os/rauc/rauc.js
+++ os/rauc/rauc.js
@@ -1,12 +1,14 @@
 #!/usr/bin/env node
 
-import { readFile, writeFile } from "node:fs/promises"
+import { readFile, writeFile, rm, mkdir } from "node:fs/promises"
 
 import { parse, stringify } from "ini"
 import { $ } from "execa"
 
 import { getBootedDevice, getBlockDevices } from "../image/lib.js"
 import { getBootedPartitionNumber } from "../image/rpi.js"
+import { fileURLToPath } from "node:url"
+import { join } from "node:path"
 
 const device = await getBootedDevice()
 
@@ -119,7 +121,29 @@ export async function setup_system_conf() {
 }
 
 export async function createBundle(device, bootname) {
+  const date = new Date().toISOString()
+  const build = date
+  const version = date.split("T")[0]
+
   const partitions = await getBlockDevices(device)
+
+  const manifest = parse(
+    await readFile(
+      fileURLToPath(import.meta.resolve("./manifest.ini")),
+      "utf8",
+    ),
+  )
+
+  const temp_dir = fileURLToPath(import.meta.resolve("./temp-dir"))
+  await rm(temp_dir, {
+    recursive: true,
+    force: true,
+  })
+  await mkdir(temp_dir)
+  const manifest_path = join(temp_dir, "manifest.raucm")
+  manifest.update.build = build
+  manifest.update.version = version
+  await writeFile(manifest_path, stringify(manifest))
 
   const part_firmware = partitions.find(
     (part) => part.partlabel === `FIRMWARE_${bootname}`,
@@ -128,13 +152,17 @@ export async function createBundle(device, bootname) {
     (part) => part.partlabel === `ROOT_${bootname}`,
   )
 
-  await $`dd if=${part_firmware.path} of=temp-dir/FIRMWARE.vfat.img bs=64M`
+  await $`dd if=${part_firmware.path} of=${join(temp_dir, manifest.image.FIRMWARE.filename)} bs=64M`
 
   // TODO: Create a img file instead with "mkfs.ext4 rootfs.img"
   // mount it as loop device "mount -o loop rootfs.img /mnt"
   // then rsync files into it
-  await $`dd if=${part_root.path} of=temp-dir/ROOT.ext4.img bs=64M`
-  await $`rauc --cert demo.cert.pem --key demo.key.pem bundle temp-dir/ update-2015.04-1.raucb`
+  await $`dd if=${part_root.path} of=${join(temp_dir, manifest.image.ROOT.filename)} bs=64M`
+  await $`rauc --cert demo.cert.pem --key demo.key.pem bundle temp-dir/ PlanktoScope-update-${version}.raucb`
+  await rm(temp_dir, {
+    recursive: true,
+    force: true,
+  })
 }
 
 if (import.meta.main) {
