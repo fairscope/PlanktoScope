e4c754bd1a2550382b53aef7c353b0c26abc0fed
Sonny Piers <sonny@fairscope.com>
2026-04-29 14:25:43 +0000

    f

--- os/raspberry/firmware.sh
+++ os/raspberry/firmware.sh
@@ -1,10 +1,10 @@
 #!/bin/bash -eux
 
+sudo mount -o remount,rw /boot/firmware
+
 # Configure firmware
 # https://www.raspberrypi.com/documentation/computers/config_txt.html
-sudo mount -o remount,rw /boot/firmware
 sudo bash -c "cat \"config.ini\" >> \"/boot/firmware/config.txt\""
-sudo mount -o remount,ro /boot/firmware
 
 # Disable the 4 Raspberry logo in the top left corner
 # more space for kernel and system logs
@@ -19,3 +19,5 @@ sudo mount -o remount,ro /boot/firmware
 [ ! -f /boot/firmware/cmdline.txt ] || \
   grep -qw logo.nologo /boot/firmware/cmdline.txt || \
   sudo sed -i 's/$/ logo.nologo/' /boot/firmware/cmdline.txt
+
+sudo mount -o remount,ro /boot/firmware
