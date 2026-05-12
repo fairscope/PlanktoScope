6991d4183ce9da64ca020a1df8af80b3c8fb4119
Sonny Piers <sonny@fairscope.com>
2026-05-12 11:29:08 +0000

    f

--- os/preimage.sh
+++ os/preimage.sh
@@ -9,9 +9,8 @@ sudo rm -f /etc/ssh/ssh_host_*_key*
 uv clean --force
 npm cache clean --force
 
+# Outdated and takes a lot of space
 rm -rf /opt/PlanktoScope/documentation
-sudo rm -rf /srv/docs
-
 
 # Clear machine-id so that it will be regenerated on the next boot
 # This is also the condition for ConditionFirstBoot=yes
