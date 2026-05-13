d164a3ba1f4aed78a181028b7a0d2e61b9898892
Sonny Piers <sonny@fairscope.com>
2026-05-13 09:00:42 +0000

    f

---
+++ os/machine-name/update-hostname.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+
+import { updateName } from "../lib/identity.js"
+
+if (import.meta.main) {
+  if (process.getuid() !== 0) {
+    throw new Error("Please run as root.")
+  }
+
+  const name = await readFile("/run/machine-name", "utf8")
+  await updateName(name)
+  process.exit(0)
+}
